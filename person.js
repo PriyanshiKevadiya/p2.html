d850c18b91b375af0d044bd292b5b9f164324aa0
Priyanshi Kevadiya <priyanshikevadiya222@gmail.com>
2022-07-18 16:38:54 +0530

    p2 javascript program

---
+++ person.js
@@ -0,0 +1,10 @@
+const name = "priyanshi";
+const age = 40;
+export{name,age};
+
+
+ // let a = ['h', 'b' ,'y' ,'e' ,'p'];
+        // let b = [...a];
+        // console.log(b);
+
+        // three dot mean insert one array in another array 
